88cc06fe1890f3d73e2639fa6a449bb9d127b9c2
huulocdev <huuloc.dev@gmail.com>
2021-12-07 20:09:21 +0700

    loc update ex45678

---
+++ Ex6.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2021 at 05:35 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `week2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items_ordered`
+--
+
+CREATE TABLE `items_ordered` (
+  `CustomerId` int(11) NOT NULL,
+  `Order_Date` date DEFAULT NULL,
+  `Item` varchar(50) DEFAULT NULL,
+  `Quantity` char(11) DEFAULT NULL,
+  `Price` char(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items_ordered`
+--
+ALTER TABLE `items_ordered`
+  ADD KEY `fk_item_id_customer` (`CustomerId`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items_ordered`
+--
+ALTER TABLE `items_ordered`
+  ADD CONSTRAINT `fk_item_id_customer` FOREIGN KEY (`CustomerId`) REFERENCES `customer` (`CustomerId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
