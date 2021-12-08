f498a4a8b8c0f21f7f34a868e5c7f3e9388208c7
xuanthong2000 <56944271+xuanthong2000@users.noreply.github.com>
2021-12-08 14:36:51 +0700

    Thong.NX push ex 8-11

---
+++ Ex8-11/Ex10.sql
@@ -0,0 +1,5 @@
+﻿--Câu 10
+SELECT item, max(price) AS GIA_CAO_NHAT, min(price) AS GIA_THAP_NHAT
+FROM items_ordered
+GROUP BY item;
+--Hiển thị price cao nhất và thấp nhất của mỗi item và sử dụng GROUP BY để không bị trùng hoặc lặp lại item.
---
+++ Ex8-11/Ex8.sql
@@ -0,0 +1,5 @@
+﻿--Câu 8
+SELECT MIN(price) AS GIA_THAP_NHAT
+FROM items_ordered
+WHERE item = 'tent';
+--Hiển thị price trị thấp nhất của item 'tent' trong bảng items_ordered
