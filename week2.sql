c5a3c1fc1d91d2ac045b00b1628df24ce286af35
minhducand2 <minhducand@gmail.com>
2021-12-08 12:02:12 +0700

    update

--- week2.sql
+++ week2.sql
@@ -156,3 +156,13 @@ SELECT order_date, item, price FROM items_ordered WHERE price BETWEEN 10.00 AND 
 
 -- test demo --
 
+-- Câu 20 
+
+-- lấy giá trị của order_date, item, price từ bảng items_ordered với điều kiện giá bán lớn hơn 10.00 và nhỏ hơn 80.00 
+SELECT order_date, item, price FROM items_ordered WHERE price BETWEEN 10.00 AND 80.00
+
+
+-- Câu 21 
+
+-- lấy giá trị của firstname, city, sate từ bảng customer và trả giá trị ra với điều kiện kết quả thuộc ('Arizona'. 'Washington', 'Oklahoma', 'Colorado', 'Hawaii');
+ SELECT firstname, city, state FROM customer WHERE state IN ('Arizona', 'Washington', 'Oklahoma', 'Colorado', 'Hawaii')
