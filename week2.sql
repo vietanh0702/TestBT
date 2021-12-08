863db396800287b4bfb220cd0402d4079cc60e9b
vietanh <vietanh7200@gmail.com>
2021-12-08 12:11:15 +0700

    VietAnh update bài tập

--- week2.sql
+++ week2.sql
@@ -127,6 +127,7 @@ ORDER BY price ASC;
 --nhóm price theo thứ tự tăng dần những price giống nhau sẽ hiển thị 1 lần--
 --Trung.TQ end--
 
+-- Việt Ánh
 -- câu 17
 -- hiển thị giá trị của item và price từ bảng items_ordered với điều kiện giá > 10.00 rồi nhóm chúng và trả ra kết quả theo thứ tự tăng dần 
 SELECT item, price FROM items_ordered WHERE price > 10.00 ORDER BY price ASC
@@ -154,15 +155,3 @@ SELECT order_date, item, price FROM items_ordered WHERE price BETWEEN 10.00 AND 
 -- lấy giá trị của firstname, city, sate từ bảng customer và trả giá trị ra với điều kiện kết quả thuộc ('Arizona'. 'Washington', 'Oklahoma', 'Colorado', 'Hawaii');
  SELECT firstname, city, state FROM customer WHERE state IN ('Arizona', 'Washington', 'Oklahoma', 'Colorado', 'Hawaii')
 
--- test demo --
-
--- Câu 20 
-
--- lấy giá trị của order_date, item, price từ bảng items_ordered với điều kiện giá bán lớn hơn 10.00 và nhỏ hơn 80.00 
-SELECT order_date, item, price FROM items_ordered WHERE price BETWEEN 10.00 AND 80.00
-
-
--- Câu 21 
-
--- lấy giá trị của firstname, city, sate từ bảng customer và trả giá trị ra với điều kiện kết quả thuộc ('Arizona'. 'Washington', 'Oklahoma', 'Colorado', 'Hawaii');
- SELECT firstname, city, state FROM customer WHERE state IN ('Arizona', 'Washington', 'Oklahoma', 'Colorado', 'Hawaii')
